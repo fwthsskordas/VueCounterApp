3d79b4cc4e65b63a0f3ae83b04c2b0cdf6b7cb05
fotis <fotis@arx.net>
2023-11-03 13:54:02 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import { ref } from "vue"
+const count = ref(0);
+
+function minus(){
+  count.value--;
+}
+
+
+function add(){
+  count.value++;
+}
+</script>
+
+<template>
+  <div>
+    <h4>
+      The Current Count is:
+    </h4>
+    <h3>{{count}}</h3>
+    <button @click="minus">-</button>
+    <button @click="add">+</button>
+  </div>
+</template>
+
+<style scoped>
+body{
+  background-color: aliceblue;
+  height: 100vh;
+  width: 99vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  }
+  
+  div{
+    text-align: center;
+  }
+</style>
